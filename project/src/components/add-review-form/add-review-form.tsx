37be8ea11d5bce048c7f667ba17fd86bc9e90051
Александр <kalyghnii@gmail.com>
2022-03-03 20:08:45 +0500

    Настроил стейт поля комментария, Настроил работу кнопки

--- project/src/components/add-review-form/add-review-form.tsx
+++ project/src/components/add-review-form/add-review-form.tsx
@@ -3,10 +3,13 @@ import { useState } from 'react';
 type RatingInputProps = {
   name: string;
   index: number;
-  onChecked: (id: string) => void;
+  value: number;
+  onRatingChange: (id: number) => void;
 };
 
-function RatingInput({ name, index, onChecked }: RatingInputProps): JSX.Element {
+function RatingInput({ name, index, value, onRatingChange }: RatingInputProps): JSX.Element {
+  console.log(typeof value);
+
   return (
     <>
       <input
@@ -15,7 +18,8 @@ function RatingInput({ name, index, onChecked }: RatingInputProps): JSX.Element 
         type="radio"
         name="rating"
         value={index}
-        onChange={() => onChecked(`star-${index}`)}
+        checked={index === value}
+        onChange={() => onRatingChange(index)}
       />
       <label className="rating__label" htmlFor={`star-${index}`}>
         {`${name} ${index}`}
@@ -24,15 +28,15 @@ function RatingInput({ name, index, onChecked }: RatingInputProps): JSX.Element 
   );
 }
 
-const createRatingContent = (elem: number, cb: (id: string) => void) => <RatingInput name="Rating" index={elem} key={elem} onChecked={cb} />;
-
-
 function AddReviewForm(): JSX.Element {
-  const [rating, setRating] = useState('null');
+  const [rating, setRating] = useState(0);
+  const [reviewText, setReviewText] = useState('');
 
-  const onUpdateRating = (id: string): void => {
+  const handleRatingChange = (id: number): void => {
     setRating(id);
+  console.log(id)
   };
+
 
   return (
     <div className="add-review">
@@ -40,17 +44,13 @@ function AddReviewForm(): JSX.Element {
         <div className="rating">
           -
           <div className="rating__stars">
-            {Array.from({ length: 10 }, (elem, i) => {
-              elem = createRatingContent(i, () => {
-                onUpdateRating(`${i + 1}`);
-              });
-            }).reverse()}
+            {Array.from({ length: 10 }, (_, i) => <RatingInput name="Rating" index={i + 1} key={i + 1} value={rating} onRatingChange={handleRatingChange} />).reverse()}
           </div>
         </div>
         <div className="add-review__text">
-          <textarea className="add-review__textarea" name="review-text" id="review-text" placeholder="Review text"></textarea>
+          <textarea className="add-review__textarea" name="review-text" id="review-text" placeholder="Review text" value={reviewText} onChange={(evt) => setReviewText(evt.target.value)}></textarea>
           <div className="add-review__submit">
-            <button className="add-review__btn" type="submit">
+            <button className="add-review__btn" type="submit" disabled={rating === 0 || reviewText.length < 50 || reviewText.length > 300}>
               Post
             </button>
           </div>
